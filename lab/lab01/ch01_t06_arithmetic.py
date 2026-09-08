7baec32d6eeee2393a644a107f816c56e9cdbc2b
KungKaYung-2627 <alankung221@gmail.com>
2026-09-08 02:56:31 +0000

    [skip actions] lab/lab01/ch01_t06_arithmetic.py

--- lab/lab01/ch01_t06_arithmetic.py
+++ lab/lab01/ch01_t06_arithmetic.py
@@ -1,9 +1,3 @@
-mirthful_addition = 12381 + 91817 
-amazing_subtraction = 981 - 312 
-trippy_multiplication = 38 * 902 
-happy_division = 540 / 45 
-sassy_combinations = 129 * 1345 + 120 / 6 - 12
-
-is_this_number_odd = 15 % 2 
-is_this_number_divisible_by_seven = 133 % 7
-
+num1 = 5
+num2 = 10
+product = num1 
