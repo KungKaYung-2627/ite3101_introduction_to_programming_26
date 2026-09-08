4fd12bd3d7d9b1d10a464848be87ee068b6c85dc
KungKaYung-2627 <alankung221@gmail.com>
2026-09-08 03:29:33 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,8 +1,5 @@
-# Hi! I'm Maria and I live in script.py.
-# I'm an expert Python coder.
-# I'm 21 years old and I plan to program cool stuff forever.
 age = 12
-is_12 = age ==12
+is_12 = age == 12
 
 name = "Maria"
 name_is_maria = name == "Maria"
