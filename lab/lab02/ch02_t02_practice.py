804c0649b5599b6904d5f316e95e2a074acc5b1d
KungKaYung-2627 <alankung221@gmail.com>
2026-09-15 02:46:05 +0000

    lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -2,9 +2,9 @@
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
-caesar to "Graham"
-Set praline to "John"
--	Set viking to "Teresa"
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
 # print(caesar)
 # print(praline)
 # print(viking)
